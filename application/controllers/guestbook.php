7dc505df882adbcc46955537e40f1b5f7e798d81
faisalsahak <sahak.faisal@gmail.com>
2017-01-27 00:39:01 -0800

    added the function to add and retrieve from database"

--- application/controllers/guestbook.php
+++ application/controllers/guestbook.php
@@ -7,7 +7,15 @@ class Guestbook extends CI_Controller {
    $data = array();
    $data["posted"] = false;
    if( $this->input->post("submit") ) {
-    
+     $data = array(
+     "name" => $this->input->post("name"),
+     "email" => $this->input->post("email"),
+     "comment" => $this->input->post("comment")
+     );
+     if( $this->Guestbook_model->insert( $data ) ) {
+       $data["posted"] = true;
+     }
+   }
   //  $data["entries"] = $this->Guestbook_model->view();
    $this->load->view("guestbook.php", $data);
  }
